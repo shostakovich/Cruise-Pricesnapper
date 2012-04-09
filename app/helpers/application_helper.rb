a043a90cd8d4cd59fe4f08b9fd5268c2428fe9fb
Robert Curth <robert@rocu.de>
2012-04-09 16:40:23 +0200

    Added ship to table and added all fields to edit action.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
       link_to title, :sort => column, :direction => direction
     end
   end
+
+  def form_field(f, field, label=nil)
+  	label = label || field
+	  content_tag(:div,
+	  	f.label(field, label, ) +
+	  	content_tag(:div,
+	  		f.text_field(field, :class => "span9"),
+	  		:class => "input"),
+	  	:class => "clearfix")
+  end
 end
